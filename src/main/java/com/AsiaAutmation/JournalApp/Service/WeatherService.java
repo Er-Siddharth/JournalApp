b3e7a7aeb68913eedd7cf283cbd6d3d183bb5fda
Er-Siddharth <124043862+Er-Siddharth@users.noreply.github.com>
2025-01-01 16:10:00 +0530

    @Value annotation use

--- src/main/java/com/AsiaAutmation/JournalApp/Service/WeatherService.java
+++ src/main/java/com/AsiaAutmation/JournalApp/Service/WeatherService.java
@@ -1,6 +1,9 @@
 package com.AsiaAutmation.JournalApp.Service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
@@ -10,12 +13,18 @@ import org.springframework.web.client.RestTemplate;
 public class WeatherService {
     @Autowired
     private RestTemplate restTemplate;
-    private static final String API_KEY="5cb41ba4e7739d0f4b9275c05ead2854";
+    @Value("${weather.api.key}")
+    private String API_KEY;
     private static final String API = "http://api.weatherstack.com/current?access_key=API_KEY&query=CITY";
 
     public WeatherResponse getResponse(String city){
         String finalApi = API.replace("API_KEY",API_KEY).replace("CITY",city);
-        ResponseEntity<WeatherResponse> response = restTemplate.exchange(finalApi, HttpMethod.GET, null, WeatherResponse.class);
-        return response.getBody();
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("username","admin");
+        httpHeaders.add("password","admin");
+        HttpEntity<String> httpEntity = new HttpEntity<>("Hello there",httpHeaders);
+        ResponseEntity<WeatherResponse> response1 = restTemplate.exchange(finalApi, HttpMethod.GET, null, WeatherResponse.class);
+        ResponseEntity<WeatherResponse> response2 = restTemplate.exchange(finalApi, HttpMethod.POST, httpEntity, WeatherResponse.class);
+        return response1.getBody();
     }
 }
