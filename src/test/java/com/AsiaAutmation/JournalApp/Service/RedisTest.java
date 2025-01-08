c15baff0fdad0578465a3415c257283a77443326
Er-Siddharth <124043862+Er-Siddharth@users.noreply.github.com>
2025-01-08 10:48:29 +0530

    Added some tests

--- src/test/java/com/AsiaAutmation/JournalApp/Service/RedisTest.java
+++ src/test/java/com/AsiaAutmation/JournalApp/Service/RedisTest.java
@@ -1,6 +1,7 @@
 package com.AsiaAutmation.JournalApp.Service;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,12 +15,23 @@ public class RedisTest {
     @Autowired
     RedisTemplate redisTemplate;
 
+    @Disabled
     @Test
     public void checkRedis(){
         Assertions.assertDoesNotThrow(()->{
             redisTemplate.opsForValue().set("email","demoEmail@gmail.com");
-            Object email = redisTemplate.opsForValue().get("email");
-            System.out.println(email.toString());
+//            Object email = redisTemplate.opsForValue().get("email");
+//            Object demo = redisTemplate.opsForValue().get("demoValue");
+//            System.out.println(email.toString());
+//            System.out.println(demo.toString());
         });
+    }
+
+    @Disabled
+    @Test
+    public void checkValues(){
+        Object email = redisTemplate.opsForValue().get("email");
+        Assertions.assertEquals("demoEmail@gmail.com", email.toString());
+        Assertions.assertEquals("Siddharth", redisTemplate.opsForValue().get("demoValue").toString());
     }
 }
